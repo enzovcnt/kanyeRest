cae37acce63b06ee79795abde71d132e7e08116e
enzo <enzovcnt@gmail.com>
2024-11-20 01:20:22 +0100

    check response = ok

--- js/main.js
+++ js/main.js
@@ -1,1 +1,9 @@
-alert("Hello World!");
+fetch('https://kanye.rest/')
+    .then(res => {
+        if (res.ok){
+            console.log('bien');
+        }else {
+            console.log('non');
+        }
+    })
+    .then(data => console.log(data));
